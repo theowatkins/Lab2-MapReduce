d065bd7eb93f0c87661d17686bc345faab35d0e4
Alberto Rodriguez <vhsalbertorodriguez@gmail.com>
2020-09-23 17:46:21 -0700

    removing unused files after single file merge

--- mrsequential.go
+++ mrsequential.go
@@ -90,29 +90,6 @@ func main() {
 	ofile.Close()
 }
 
-//
-// load the application Map and Reduce functions
-// from a plugin file, e.g. ../mrapps/wc.so
-//
-//func loadPlugin(filename string) (func(string, string) []mr.KeyValue, func(string, []string) string) {
-//	p, err := plugin.Open(filename)
-//	if err != nil {
-//		log.Fatalf("cannot load plugin %v", filename)
-//	}
-//	xmapf, err := p.Lookup("Map")
-//	if err != nil {
-//		log.Fatalf("cannot find Map in %v", filename)
-//	}
-//	mapf := xmapf.(func(string, string) []mr.KeyValue)
-//	xreducef, err := p.Lookup("Reduce")
-//	if err != nil {
-//		log.Fatalf("cannot find Reduce in %v", filename)
-//	}
-//	reducef := xreducef.(func(string, []string) string)
-//
-//	return mapf, reducef
-//}
-
 func Map(filename string, contents string) []mr.KeyValue {
 	// function to detect word separators.
 	ff := func(r rune) bool { return !unicode.IsLetter(r) }
