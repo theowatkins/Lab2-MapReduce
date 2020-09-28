6dc6bb82a3596da7b77b26808f9249fb8f2c7d0d
Theo <thwatkin@calpoly.edu>
2020-09-28 13:57:22 -0400

    modified start.sh to run mrdistributed.go

--- start.sh
+++ start.sh
@@ -1,2 +1,1 @@
-go run main/types.go main/jobs.go main/filehelper.go main/mrsequential.go pg*.txt
-more mr-out-0
+go run main/types.go main/jobs.go main/filehelper.go main/functions.go main/mrdistributed.go pg*.txt
